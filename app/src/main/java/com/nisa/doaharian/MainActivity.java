58d5c1ca11236fec0eea04ad5f39905e5108c1df
nisahani <nurrochmah.hani98@gmail.com>
2019-10-13 14:25:49 +0700

    bismillah

--- app/src/main/java/com/nisa/doaharian/MainActivity.java
+++ app/src/main/java/com/nisa/doaharian/MainActivity.java
@@ -1,6 +1,8 @@
 package com.nisa.doaharian;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, MainMenuActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
     }
 }
